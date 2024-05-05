f2a8a5df8dba9f0d62ddbf6710c417297851ad74
gurcxynski <woj.gorczynski@gmail.com>
2024-05-05 23:35:29 +0200

    created basic mouse handling

--- src/Core/Piece.cs
+++ src/Core/Piece.cs
@@ -6,24 +6,32 @@ namespace Chess.Core
 {
     internal class Piece
     {
-        internal PieceType Type { get; init; }
-        internal Vector2 GridPosition { get; }
-        internal Vector2 DrawPosition => GridPosition * 100;
-        internal Piece(PieceType type, Vector2 position)
+        internal PieceType Type { get; }
+        internal Vector2 GridPosition { get; private set; }
+        internal Vector2 DrawPosition => 100 * GridPosition; //* new Vector2(1, -1) + 7 * Vector2.UnitY);
+        internal Vector2 DrawPositionCentered => DrawPosition + new Vector2(50, 50);
+        internal bool IsWhite { get; }
+        internal Piece(PieceType type, Vector2 position, bool isWhite = true)
         {
             Type = type;
             GridPosition = position;
+            IsWhite = isWhite;
         }
         internal void Draw(SpriteBatch spriteBatch)
         {
             Texture2D texture = Game1.self.textures.Get(Type.ToString());
             Rectangle destinationRectangle = new((int)DrawPosition.X, (int)DrawPosition.Y, 100, 100);
-            spriteBatch.Draw(texture, destinationRectangle, texture.Bounds, Color.White);
+            spriteBatch.Draw(texture, destinationRectangle, texture.Bounds, IsWhite ? Color.White : Color.Red);
         }
 
         internal void Update(GameTime gameTime)
         {
 
+        }
+
+        internal void Move(Vector2 pos)
+        {
+            GridPosition = pos;
         }
     }
 
--- src/Core/Scene.cs
+++ src/Core/Scene.cs
@@ -12,33 +12,42 @@ internal class Scene {
     private void FillBoard() {
         for (int i = 0; i < 8; i++) {
             pieces.Add(new Piece(PieceType.Pawn, new Vector2(i, 1)));
-            pieces.Add(new Piece(PieceType.Pawn, new Vector2(i, 6)));
+            pieces.Add(new Piece(PieceType.Pawn, new Vector2(i, 6), false));
         }
         pieces.Add(new Piece(PieceType.Rook, new Vector2(0, 0)));
         pieces.Add(new Piece(PieceType.Rook, new Vector2(7, 0)));
-        pieces.Add(new Piece(PieceType.Rook, new Vector2(0, 7)));
-        pieces.Add(new Piece(PieceType.Rook, new Vector2(7, 7)));
         pieces.Add(new Piece(PieceType.Knight, new Vector2(1, 0)));
         pieces.Add(new Piece(PieceType.Knight, new Vector2(6, 0)));
-        pieces.Add(new Piece(PieceType.Knight, new Vector2(1, 7)));
-        pieces.Add(new Piece(PieceType.Knight, new Vector2(6, 7)));
         pieces.Add(new Piece(PieceType.Bishop, new Vector2(2, 0)));
         pieces.Add(new Piece(PieceType.Bishop, new Vector2(5, 0)));
-        pieces.Add(new Piece(PieceType.Bishop, new Vector2(2, 7)));
-        pieces.Add(new Piece(PieceType.Bishop, new Vector2(5, 7)));
         pieces.Add(new Piece(PieceType.Queen, new Vector2(3, 0)));
-        pieces.Add(new Piece(PieceType.Queen, new Vector2(3, 7)));
         pieces.Add(new Piece(PieceType.King, new Vector2(4, 0)));
-        pieces.Add(new Piece(PieceType.King, new Vector2(4, 7)));
+        
+        pieces.Add(new Piece(PieceType.Rook, new Vector2(0, 7), false));
+        pieces.Add(new Piece(PieceType.Rook, new Vector2(7, 7), false));
+        pieces.Add(new Piece(PieceType.Knight, new Vector2(1, 7), false));
+        pieces.Add(new Piece(PieceType.Knight, new Vector2(6, 7), false));
+        pieces.Add(new Piece(PieceType.Bishop, new Vector2(2, 7), false));
+        pieces.Add(new Piece(PieceType.Bishop, new Vector2(5, 7), false));
+        pieces.Add(new Piece(PieceType.Queen, new Vector2(3, 7), false));
+        pieces.Add(new Piece(PieceType.King, new Vector2(4, 7), false));
     }
     internal Scene() {
         FillBoard();
         mouseListener.MouseClicked += (sender, args) => {
             var mousePosition = args.Position;
+            Console.WriteLine(mousePosition);
+            if (selectedPiece is not null) {
+                Vector2 clicked = new(mousePosition.X / 100, mousePosition.Y / 100);
+                
+                selectedPiece.Move(clicked);
+                selectedPiece = null;
+                return;
+            }
             foreach (var piece in pieces) {
-                if (Vector2.Distance(piece.GridPosition * 100, mousePosition.ToVector2()) < 50) {
+                if (Vector2.Distance(piece.DrawPositionCentered, mousePosition.ToVector2()) < 50) {
                     selectedPiece = piece;
-                    break;
+                    return;
                 }
             }
         };
