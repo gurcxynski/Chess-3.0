533a8cf51c576b721fe12bd13b8726f115eb20a1
gurcxynski <woj.gorczynski@gmail.com>
2025-07-02 14:34:32 +0200

    moved the interface and framework from windows directx app to a blazor web app

---
+++ Backend/Engine/Pieces/Queen.cs
@@ -0,0 +1,9 @@
+using Chess.Engine;
+using System.Numerics;
+using System;
+
+namespace Chess.Engine.Pieces;
+internal class Queen(Vector2 position, bool isWhite = true) : Piece(position, isWhite)
+{
+    protected override bool CheckBasicMovement(Vector2 direction, Board board) => Math.Abs(direction.X) == Math.Abs(direction.Y) || direction.X == 0 || direction.Y == 0;
+}
---
+++ Backend/Engine/Pieces/Rook.cs
@@ -0,0 +1,8 @@
+using Chess.Engine;
+using System.Numerics;
+
+namespace Chess.Engine.Pieces;
+internal class Rook(Vector2 position, bool isWhite = true) : Piece(position, isWhite)
+{
+    protected override bool CheckBasicMovement(Vector2 direction, Board board) => direction.X == 0 || direction.Y == 0;
+}
